21fdc205262f8cd93bc86340b4efdb9c09fe1eec
Siddarth <siddarthrgovindan@gmail.com>
2019-04-15 14:16:15 -0400

    init project files

---
+++ CodeLabMidtermClass/Assets/Scripts/SavePosition.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class SavePosition : MonoBehaviour
+{
+    string filePath;
+    public int[] highScores;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        highScores = new[] {4, 5, 6, 7, 8, 8};
+        string spString = JsonUtility.ToJson(this, true);
+        print(spString);
+        File.WriteAllText(Application.dataPath + "/spStr.json",spString);
+        
+       // JsonUtility.FromJsonOverwrite(Application.dataPath+"/spStr.json",this);
+        
+        
+        filePath = Application.dataPath + "/pos.json";
+        if (File.Exists(filePath))
+        {
+            string contents = File.ReadAllText(filePath);
+            transform.position = JsonUtility.FromJson<Vector3>(contents);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Vector3 position = transform.position;
+            string posStr = JsonUtility.ToJson(position, true);
+            print(posStr);
+            File.WriteAllText(filePath,posStr);
+        }
+    }
+}
---
+++ CodeLabW8/Assets/CameraDemo.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+// USAGE: put on your main camera
+// INTENT: demo some camera control techniques,
+// like simple mouse look / following a target
+public class CameraDemo : MonoBehaviour
+{
+    void Update()
+    {
+        // simple mouse look
+        
+        // 1. get mouse input 
+        float horizontalMouseSpeed = Input.GetAxis("Mouse X");
+        float verticalMouseSpeed = Input.GetAxis("Mouse Y");
+        Debug.Log(horizontalMouseSpeed);
+
+        // 2. use mouse input to rotate camera
+        // PITCH - x, YAW - y, ROLL - z 
+        transform.Rotate(0f, horizontalMouseSpeed,0f);
+        Camera.main.transform.Rotate(-verticalMouseSpeed,0f,0f);
+        // 3. unroll the camera, we need to set its z to 0f, always
+        //transform.eulerAngles.z = 0f //Unity won't let you do it like this! Because of a C# limitation
+        transform.eulerAngles = new Vector3(transform.eulerAngles.x,transform.eulerAngles.y,0f);
+        
+        // 4. profit
+    }
+}
