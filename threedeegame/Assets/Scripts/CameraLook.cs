fffb2e13c63817fc58324c8ce91d7d8518183ad6
Siddarth <siddarthrgovindan@gmail.com>
2019-04-22 01:04:46 -0400

    rewrote player movement and mouse look

--- threedeegame/Assets/Scripts/CameraLook.cs
+++ threedeegame/Assets/Scripts/CameraLook.cs
@@ -3,18 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraLook : MonoBehaviour
-{ 
+{
+    public GameObject player;
+    public float rotationSpeed;
     void Update()
     {
         //get mouse input
-        float horizontalSpeed = Input.GetAxis("Mouse X");
-        float verticalSpeed = Input.GetAxis("Mouse Y");
+        float horizontalSpeed = Input.GetAxis("Mouse X") * rotationSpeed;
+        float verticalSpeed = Input.GetAxis("Mouse Y") * rotationSpeed;
 
         //rotate camera using input
-        Camera.main.transform.Rotate(0f,horizontalSpeed,0f);
-        Camera.main.transform.Rotate(-verticalSpeed,0f,0f);
+        player.transform.Rotate(0f,horizontalSpeed,0f);
+        player.transform.Rotate(-verticalSpeed,0f,0f);
         
         //lock the Z axis rotation 
-        transform.eulerAngles= new Vector3(transform.eulerAngles.x,transform.eulerAngles.y,0f);
+        player.transform.eulerAngles= new Vector3(transform.eulerAngles.x,transform.eulerAngles.y,0f);
     }
 }
---
+++ threedeegame/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed;
+    private Rigidbody rb;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        //get inputs
+        float hAxis = Input.GetAxis("Horizontal");
+        float vAxis = Input.GetAxis("Vertical");
+        //calculate movement
+        Vector3 movement = (transform.forward * vAxis + transform.right * hAxis) * moveSpeed * Time.deltaTime;
+        //update position
+        rb.MovePosition(transform.position + movement);
+    }
+}
