1763682979b13fd25ee1fe967ce58fbcd3d063b3
Siddarth <siddarthrgovindan@gmail.com>
2019-04-22 00:17:06 -0400

    immported more 3d model assets, wrote camera mouse look script

--- threedeegame/Assets/Scripts/CameraLook.cs
+++ threedeegame/Assets/Scripts/CameraLook.cs
@@ -3,16 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraLook : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+{ 
     void Update()
     {
+        //get mouse input
+        float horizontalSpeed = Input.GetAxis("Mouse X");
+        float verticalSpeed = Input.GetAxis("Mouse Y");
+
+        //rotate camera using input
+        Camera.main.transform.Rotate(0f,horizontalSpeed,0f);
+        Camera.main.transform.Rotate(-verticalSpeed,0f,0f);
         
+        //lock the Z axis rotation 
+        transform.eulerAngles= new Vector3(transform.eulerAngles.x,transform.eulerAngles.y,0f);
     }
 }
