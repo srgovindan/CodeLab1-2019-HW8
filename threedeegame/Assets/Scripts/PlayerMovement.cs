f866d556af2594255b912a6d7cdec979f9c81e38
Siddarth <siddarthrgovindan@gmail.com>
2019-04-22 01:19:09 -0400

    wrote an interact function that raycasts to check for an object

--- threedeegame/Assets/Scripts/PlayerMovement.cs
+++ threedeegame/Assets/Scripts/PlayerMovement.cs
@@ -13,6 +13,15 @@ public class PlayerMovement : MonoBehaviour
 
     void Update()
     {
+        Move();
+        if (Input.GetMouseButton(0))
+        {
+            Interact();
+        }
+    }
+
+    void Move()
+    {
         //get inputs
         float hAxis = Input.GetAxis("Horizontal");
         float vAxis = Input.GetAxis("Vertical");
@@ -20,5 +29,22 @@ public class PlayerMovement : MonoBehaviour
         Vector3 movement = (transform.forward * vAxis + transform.right * hAxis) * moveSpeed * Time.deltaTime;
         //update position
         rb.MovePosition(transform.position + movement);
+    }
+
+    void Interact()
+    {
+        //generate a ray
+        Ray myRay = new Ray(transform.position, transform.forward);
+        //debug draw the ray
+        Debug.DrawRay(myRay.origin,myRay.direction*1000f,Color.yellow);
+        //init a raycasthit var
+        RaycastHit myRayCastHit = new RaycastHit();
+        //raycast!
+        if (Physics.Raycast(myRay, out myRayCastHit, 1000f))
+        {
+            //do stuff with the raycast
+            
+        }
+
     }
 }
